c28f7b8bd915cd83c193f86a3c555dbfcd49e220
Camila Bustos <camilabustos@google.com>
2026-01-16 15:31:20 -0800

    chore: downgrade kustomize to v5.4.2-gke.1 (#1995) (#2003)

--- pkg/hydrate/tool_util.go
+++ pkg/hydrate/tool_util.go
@@ -47,7 +47,7 @@ const (
 	// HelmVersion is the recommended version of Helm for hydration.
 	HelmVersion = "v3.18.6-gke.1"
 	// KustomizeVersion is the recommended version of Kustomize for hydration.
-	KustomizeVersion = "v5.8.0-gke.2"
+	KustomizeVersion = "v5.4.2-gke.1"
 	// Helm is the binary name of the installed Helm.
 	Helm = "helm"
 	// Kustomize is the binary name of the installed Kustomize.
